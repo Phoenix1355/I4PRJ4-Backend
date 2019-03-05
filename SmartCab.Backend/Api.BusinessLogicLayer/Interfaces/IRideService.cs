1e00dff644dc1f36c4b8c95d0a8da2e66e3f7d5c
Michael <michael.moeller84@gmail.com>
2019-03-05 11:00:25 +0100

    Init of basic BussinessLogicLayer

---
+++ SmartCab.Backend/Api.BusinessLogicLayer/Interfaces/IRideService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.DataAccessLayer.Models;
+
+namespace Api.BusinessLogicLayer.Interfaces
+{
+    public interface IRideService
+    {
+        Task<List<Ride>> GetAllRidesAsync();
+        Task<Ride> GetRideByIdAsync(int id);
+        Task<Ride> AddRideAsync(Ride ride);
+        Task<Ride> UpdateRideAsync(Ride ride);
+        Task<Ride> DeleteRideAsync(int id);
+    }
+}
