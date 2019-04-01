4ac2e7316e6b95da69c6ecbce2ef621a2b3d315d
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-04-01 16:39:03 +0200

    Added RideStatus

---
+++ SmartCab.Backend/Api.DataAccessLayer/Migrations/20190401143532_RideStatus-added.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.DataAccessLayer.Migrations
+{
+    public partial class RideStatusadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RideStatus",
+                table: "Rides",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RideStatus",
+                table: "Rides");
+        }
+    }
+}
