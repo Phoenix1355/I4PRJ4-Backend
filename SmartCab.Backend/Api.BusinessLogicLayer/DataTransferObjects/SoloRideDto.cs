7183b42145df946aaf61003963c3b0e960309bc4
Michael <michael.moeller84@gmail.com>
2019-04-02 14:47:02 +0200

    uncommented some stuff in soloridedto

--- SmartCab.Backend/Api.BusinessLogicLayer/DataTransferObjects/SoloRideDto.cs
+++ SmartCab.Backend/Api.BusinessLogicLayer/DataTransferObjects/SoloRideDto.cs
@@ -6,11 +6,11 @@ namespace Api.BusinessLogicLayer.DataTransferObjects
 {
     public class SoloRideDto
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public Address StartDestination { get; set; }
         public Address EndDestination { get; set; }
         public DateTime DepartureTime { get; set; }
-        public decimal Price { get; set; }
+        //public decimal Price { get; set; }
         public int PassengerCount { get; set; }
         public DateTime ConfirmationDeadLine { get; set; }
     }
