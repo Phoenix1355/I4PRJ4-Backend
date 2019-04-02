89d89f90301912fb01d3b90f5033fdcc0993b686
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-04-02 19:12:55 +0200

    Added Required CustomerId to ride.

---
+++ SmartCab.Backend/Api.DataAccessLayer/Migrations/20190402171120_AddedRequiredCustomerIdToRide.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.DataAccessLayer.Migrations
+{
+    public partial class AddedRequiredCustomerIdToRide : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rides_AspNetUsers_CustomerId",
+                table: "Rides");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rides_CustomerId",
+                table: "Rides");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Rides",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerId1",
+                table: "Rides",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_CustomerId1",
+                table: "Rides",
+                column: "CustomerId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rides_AspNetUsers_CustomerId1",
+                table: "Rides",
+                column: "CustomerId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rides_AspNetUsers_CustomerId1",
+                table: "Rides");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rides_CustomerId1",
+                table: "Rides");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId1",
+                table: "Rides");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerId",
+                table: "Rides",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_CustomerId",
+                table: "Rides",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rides_AspNetUsers_CustomerId",
+                table: "Rides",
+                column: "CustomerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
