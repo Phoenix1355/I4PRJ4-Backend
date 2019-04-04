2e9f5275125d61e0feef6d2b09d147c8a1c09527
Michael Møller-Hansen <michael.moeller84@gmail.com>
2019-04-04 18:42:14 +0200

    Error fail FTW

--- SmartCab.Backend/Api/Errorhandling/CustomExceptionFilter.cs
+++ SmartCab.Backend/Api/Errorhandling/CustomExceptionFilter.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Net;
 using Api.BusinessLogicLayer.Responses;
