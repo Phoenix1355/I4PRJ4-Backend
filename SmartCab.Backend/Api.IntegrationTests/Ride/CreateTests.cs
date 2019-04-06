4a8c60ec669652c2f201752001315bf44cbf7d18
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-04-06 12:42:40 +0200

    Test added - How should authentication be set. Next issue.

--- SmartCab.Backend/Api.IntegrationTests/Ride/CreateTests.cs
+++ SmartCab.Backend/Api.IntegrationTests/Ride/CreateTests.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Api.BusinessLogicLayer.Requests;
+using Api.BusinessLogicLayer.Responses;
+using Api.DataAccessLayer.Models;
+using Microsoft.AspNetCore.Diagnostics;
 using NUnit.Framework;
 
 namespace Api.IntegrationTests.Ride
@@ -11,18 +16,96 @@ namespace Api.IntegrationTests.Ride
     class CreateTests : IntegrationSetup
     {
         [Test]
-        public async Task Create_WhenCreateIsCalled_RideIsCreated()
+        public async Task Create_WhenAuthorizedUserCallCreateWithValidRequest_RideIsCreated()
         {
-            var request = new CreateRideRequest()
+            //Create customer
+            var registerRequest = getRegisterRequest();
+            await PostAsync("/api/customer/register", registerRequest);
+
+            //Login on customer
+            var loginRequest = getLoginRequest();
+            var loginResponse = await PostAsync("/api/customer/login", loginRequest);
+
+            //Map login returned to object
+            var loginResponseObject = GetObject<LoginResponse>(loginResponse);
+
+            //Get Token
+            var token = loginResponseObject.Token;
+
+            //Create Ride Request
+            var request = getCreateRideRequest();
+
+            //Default header authentication setup.
+            _client.DefaultRequestHeaders.Add("authorization", token);
+            
+            //Make response
+           var response = await PostAsync("api/rides/create", request);
+           Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+
+        [Test]
+        public async Task Create_WhenUnauthorizedUserCallCreateWithValidRequest_RideIsNotCreated()
+        {
+            var request = getCreateRideRequest();
+            var response = await PostAsync("api/rides/create", request);
+
+            //No rides exist.
+            using (var context = _factory.CreateContext())
+            {
+                Assert.That(context.Rides.ToList().Count, Is.EqualTo(0));
+            }
+        }
+
+        [Test]
+        public async Task Create_WhenUnauthorizedUserCallCreateWithValidRequest_ErrorMessageReturned()
+        {
+            var request = getCreateRideRequest();
+            var response = await PostAsync("api/rides/create", request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public async Task Create_WhenAuthorizedUserCallCreateWithValidRequestButInsufficientFunds_RideIsNotCreated()
+        {
+
+        }
+
+        [Test]
+        public async Task Create_WhenAuthorizedUserCallCreateWithValidRequestButInsufficientFunds_ErrorMessageReturned()
+        {
+
+        }
+
+        [Test]
+        public async Task Create_WhenAuthorizedUserCallCreateWithInvalidRequest_RideIsNotCreated()
+        {
+
+        }
+
+        [Test]
+        public async Task Create_WhenAuthorizedUserCallCreateWithInvalidRequest_ErrorMessageReturned()
+        {
+
+        }
+
+        [Test]
+        public async Task Create_WhenAuthorizedUserCallsSameRequestSeveralTimes_SeveralRidesIsCreated()
+        {
+
+        }
+
+        private CreateRideRequest getCreateRideRequest()
+        {
+            return new CreateRideRequest()
             {
                 ConfirmationDeadline = DateTime.Now,
                 DepartureTime = DateTime.Now,
-                
+                StartDestination = new Address("City", 0000, "Street", 21),
+                EndDestination = new Address("City", 0000, "Street", 21),
+                IsShared = false,
+                PassengerCount = 2
             };
-           var response = await PostAsync("api/rides/create", request);
-
-           Console.WriteLine(await response.Content.ReadAsStringAsync());
         }
-
     }
 }
