17a651f7eed839cedf41db0eb00eea83a0bc69f6
Michael Møller-Hansen <michael.moeller84@gmail.com>
2019-04-06 19:59:10 +0200

    Validation of addresses added.

--- SmartCab.Backend/Api.BusinessLogicLayer/Services/GoogleMapsApiService.cs
+++ SmartCab.Backend/Api.BusinessLogicLayer/Services/GoogleMapsApiService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Web;
 using Api.BusinessLogicLayer.Interfaces;
 using Api.BusinessLogicLayer.Responses;
+using CustomExceptions;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -19,20 +20,22 @@ namespace Api.BusinessLogicLayer.Services
     /// </summary>
     public class GoogleMapsApiService : IGoogleMapsApiService
     {
-        private readonly string _url;
-        private readonly string _key;
+        private readonly string _distanceMatrixUrl;
+        private readonly string _geocodingUrl;
+        private readonly string _apiKey;
 
         public GoogleMapsApiService(IConfiguration config)
         {
-            _url = "https://maps.googleapis.com/maps/api/distancematrix/json";
-            _key = config["GoogleMapsApiKey"];
+            _distanceMatrixUrl = "https://maps.googleapis.com/maps/api/distancematrix/json";
+            _geocodingUrl = "https://maps.googleapis.com/maps/api/geocode/json";
+            _apiKey = config["GoogleMapsApiKey"];
         }
 
-        public async Task<GoogleMapsApiResponse> GetDistance(string[] originAddresses, string[] destinationAddresses)
+        public async Task<decimal> GetDistanceInKm(string origin, string destination)
         {
-            using (var client = new HttpClient())
+            using(var client = new HttpClient())
             {
-                var uri = new Uri(GetRequestUrl(originAddresses, destinationAddresses));
+                var uri = new Uri(GetDistanceMatrixRequestUrl(origin, destination));
 
                 var response = await client.GetAsync(uri);
                 if (!response.IsSuccessStatusCode)
@@ -41,17 +44,61 @@ namespace Api.BusinessLogicLayer.Services
                 }
 
                 var content = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<GoogleMapsApiResponse>(content);
+                var distanceMatrix = JsonConvert.DeserializeObject<GoogleDistanceMatrixResponse>(content);
+                
+                //Distance is returned in meters, so we divide with 1000 to get the result in kilometers
+                var distanceInKm = Convert.ToDecimal(distanceMatrix
+                                                     .Rows
+                                                     .FirstOrDefault()?
+                                                     .Elements.FirstOrDefault()?
+                                                     .Distance.Value / 1000.0);
+
+                if (distanceMatrix.Status != "OK" || distanceInKm <= 0)
+                {
+                    throw new GoogleMapsApiException("A route between the provided addresses could not be calculated.");
+                }
+
+                return distanceInKm;
             }
         }
 
-        private string GetRequestUrl(string[] originAddresses, string[] destinationAddresses)
+        public async Task ValidateAddress(string address)
         {
-            originAddresses = originAddresses.Select(HttpUtility.UrlEncode).ToArray();
-            var origins = string.Join("|", originAddresses);
-            destinationAddresses = destinationAddresses.Select(HttpUtility.UrlEncode).ToArray();
-            var destinations = string.Join("|", destinationAddresses);
-            return $"{_url}?origins={origins}&destinations={destinations}&region=dk&key={_key}";
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri(GetGeocodingRequestUrl(address));
+                var response = await client.GetAsync(uri);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("GoogleDistanceMatrixApi failed with status code: " + response.StatusCode);
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+                var geocodingResponse = JsonConvert.DeserializeObject<GoogleGeocodingResponse>(content);
+                var locationType = geocodingResponse
+                                   .Results.FirstOrDefault()?
+                                   .Geometry
+                                   .LocationType;
+
+                //"ROOFTOP" indicates the address is precise down to street level
+                //"RANGE_INTERPOLATED" indicates the result reflects an approximation (usually on a road)
+                //interpolated between two precise points (such as intersections). This is good enough
+                //https://developers.google.com/maps/documentation/geocoding/intro#reverse-restricted
+                if (locationType != "ROOFTOP" && locationType != "RANGE_INTERPOLATED")
+                {
+                    throw new GoogleMapsApiException($"The address \"{address}\" is not valid.");
+                }
+            }
+        }
+
+        private string GetDistanceMatrixRequestUrl(string origin, string destination)
+        {
+            return $"{_distanceMatrixUrl}?origins={origin}&destinations={destination}&region=dk&key={_apiKey}";
+        }
+
+        private string GetGeocodingRequestUrl(string address)
+        {
+            return $"{_geocodingUrl}?address={address}&key={_apiKey}";
         }
     }
 }
