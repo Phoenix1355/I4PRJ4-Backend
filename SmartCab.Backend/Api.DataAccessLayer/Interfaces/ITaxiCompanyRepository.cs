d41bd11d2b224df595387ae01702c50f66a373b5
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-04-08 15:47:26 +0200

    small change, Molzen should be able to use it now

--- SmartCab.Backend/Api.DataAccessLayer/Interfaces/ITaxiCompanyRepository.cs
+++ SmartCab.Backend/Api.DataAccessLayer/Interfaces/ITaxiCompanyRepository.cs
@@ -6,7 +6,7 @@ namespace Api.DataAccessLayer.Interfaces
 {
     public interface ITaxiCompanyRepository
     {
-        Task<TaxiCompany> AddTaxiCompanyAsync(TaxiCompany taxiCompany, string password);
+        //Task<TaxiCompany> AddTaxiCompanyAsync(TaxiCompany taxiCompany, string password);
         Task<TaxiCompany> GetTaxiCompanyAsync(string email);
         Task DepositAsync(string customerId, decimal deposit);
     }
