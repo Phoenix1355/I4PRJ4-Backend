c18b61689a49c18ef7624bf982bdabb1911a7cb8
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-04-08 17:01:27 +0200

    Beginning to write tests for API

---
+++ SmartCab.Backend/Api.UnitTests/Controllers/TaxiCompanyControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Api.BusinessLogicLayer.Interfaces;
+using Api.BusinessLogicLayer.Requests;
+using Api.BusinessLogicLayer.Responses;
+using Api.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.Extensions;
+using NUnit.Framework;
+
+namespace Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class TaxiCompanyControllerTests
+    {
+        #region Setup
+
+        private ITaxiCompanyService _taxiCompanyService;
+        private TaxiCompanyController _taxiCompanyController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _taxiCompanyService = Substitute.For<ITaxiCompanyService>();
+            _taxiCompanyController = new TaxiCompanyController(_taxiCompanyService);
+        }
+
+        #endregion
+    }
+}
