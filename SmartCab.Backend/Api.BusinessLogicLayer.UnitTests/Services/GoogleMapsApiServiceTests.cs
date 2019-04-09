8974cb9eb77fb4d6bff207c90b74bc3ad38a93c4
Michael <michael.moeller84@gmail.com>
2019-04-09 11:04:09 +0200

    Test of GetDistanceInKmAsync done

--- SmartCab.Backend/Api.BusinessLogicLayer.UnitTests/Services/GoogleMapsApiServiceTests.cs
+++ SmartCab.Backend/Api.BusinessLogicLayer.UnitTests/Services/GoogleMapsApiServiceTests.cs
@@ -22,6 +22,10 @@ namespace Api.BusinessLogicLayer.UnitTests.Services
         private const string GoogleMapsApiKey = "GoogleMapsApiKey";
         private const string GoogleMapsApiKeyValue = "Some-secret-key";
 
+        private const string OkStatus = "OK";
+        private const string NotOkStatus = "NOT OK";
+        private const double ValidDistance = 1000; //1 km
+
         private IHttpClient _client;
         private GoogleMapsApiService _googleMapsApiService;
 
@@ -53,15 +57,65 @@ namespace Api.BusinessLogicLayer.UnitTests.Services
         [Test]
         public void GetDistanceInKmAsync_DistanceMatrixNotOk_ThrowsGoogleMapsApiException()
         {
-            var distanceMatrixStatus = "Something else than 'OK'";
             var response = new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
-            response.Content = new StringContent("{\n   \"destination_addresses\" : [ \"Ridderstræde 1, 8000 Aarhus, Denmark\" ],\n   \"origin_addresses\" : [ \"Lundbyesgade 8, 8000 Aarhus, Denmark\" ],\n   \"rows\" : [\n      {\n         \"elements\" : [\n            {\n               \"distance\" : {\n                  \"text\" : \"2.0 km\",\n                  \"value\" : 1950\n               },\n               \"duration\" : {\n                  \"text\" : \"9 mins\",\n                  \"value\" : 528\n               },\n               \"status\" : \"OK\"\n            }\n         ]\n      }\n   ],\n   \"status\" : \"" + distanceMatrixStatus + "\"\n}\n");
+            response.Content = new StringContent(GetGoogleMapsRequestContent(OkStatus, NotOkStatus, ValidDistance));
 
             _client.GetAsync(null).ReturnsForAnyArgs(response);
             var startDest = "Some address";
             var endDest = "Another address";
 
             Assert.That(() => _googleMapsApiService.GetDistanceInKmAsync(startDest, endDest), Throws.TypeOf<GoogleMapsApiException>());
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-1000)]
+        [TestCase(-10000)]
+        [TestCase(-100000)]
+        public void GetDistanceInKmAsync_InvalidDistanceInDistanceMatrix_ThrowsGoogleMapsApiException(double distanceInMeters)
+        {
+            var response = new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
+            response.Content = new StringContent(GetGoogleMapsRequestContent(OkStatus, OkStatus, distanceInMeters));
+
+            _client.GetAsync(null).ReturnsForAnyArgs(response);
+            var startDest = "Some address";
+            var endDest = "Another address";
+
+            Assert.That(() => _googleMapsApiService.GetDistanceInKmAsync(startDest, endDest), Throws.TypeOf<GoogleMapsApiException>());
+        }
+
+        [TestCase(1)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        public async Task GetDistanceInKmAsync_ValidDistanceInDistanceMatrix_ReturnsTheDistance(double distanceInMeters)
+        {
+            var response = new HttpResponseMessage { StatusCode = HttpStatusCode.OK };
+            response.Content = new StringContent(GetGoogleMapsRequestContent(OkStatus, OkStatus, distanceInMeters));
+
+            _client.GetAsync(null).ReturnsForAnyArgs(response);
+            var startDest = "Some address";
+            var endDest = "Another address";
+
+            var returnedDistance = await _googleMapsApiService.GetDistanceInKmAsync(startDest, endDest);
+
+            Assert.That(returnedDistance, Is.EqualTo(distanceInMeters / 1000));
+        }
+
+        /// <summary>
+        /// Returns a string that represents the content of a http response from the Google Maps Api returning af single row of data.
+        /// <remarks>
+        /// Such a response has two status'. One for the entire response. If it's a valid response the status will be 'OK'.<br/>
+        /// Each row in the matrix also has a status. If the row is valid the status will be 'OK'.
+        /// </remarks>
+        /// </summary>
+        /// <param name="googleMapsReponseStatus">The status of the entire response.</param>
+        /// <param name="distanceMatrixStatus">The status of the first row.</param>
+        /// <param name="distance">The distance returned for the row.</param>
+        /// <returns></returns>
+        private string GetGoogleMapsRequestContent(string googleMapsReponseStatus, string distanceMatrixStatus, double distance)
+        {
+            return "{\n   \"destination_addresses\" : [ \"Ridderstræde 1, 8000 Aarhus, Denmark\" ],\n   \"origin_addresses\" : [ \"Lundbyesgade 8, 8000 Aarhus, Denmark\" ],\n   \"rows\" : [\n      {\n         \"elements\" : [\n            {\n               \"distance\" : {\n                  \"text\" : \"2.0 km\",\n                  \"value\" : " + distance + "\n               },\n               \"duration\" : {\n                  \"text\" : \"9 mins\",\n                  \"value\" : 528\n               },\n               \"status\" : \"" + googleMapsReponseStatus + "\"\n            }\n         ]\n      }\n   ],\n   \"status\" : \"" + distanceMatrixStatus + "\"\n}\n";
         }
     }
 }
