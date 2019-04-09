9366960ed58fdc650d28f36a01fc25daa0d2c922
Michael Møller-Hansen <michael.moeller84@gmail.com>
2019-04-09 16:40:00 +0200

    UC10 and UC12 api-layer and business-layer now 100% tested

---
+++ SmartCab.Backend/Api.UnitTests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Api.BusinessLogicLayer.Interfaces;
+using Api.BusinessLogicLayer.Responses;
+using Api.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class OrderControllerTests
+    {
+        #region Setup and fields
+
+        private IOrderService _orderService;
+        private OrderController _orderController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _orderService = Substitute.For<IOrderService>();
+            _orderController = new OrderController(_orderService);
+        }
+
+        #endregion
+
+        [Test]
+        public async Task Open_Success_ReturnsOkResponse()
+        {
+            _orderService.GetOpenOrdersAsync().Returns(new OpenOrdersResponse());
+
+            var response = await _orderController.Open(null) as ObjectResult;
+
+            Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
+        }
+    }
+}
