8cb1196a62cdc7abbcce4a7b056e0c443e9c051c
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-04-09 18:46:27 +0200

    Added Controller Deposit tests

--- SmartCab.Backend/Api/Controllers/CustomerController.cs
+++ SmartCab.Backend/Api/Controllers/CustomerController.cs
@@ -138,7 +138,7 @@ namespace Api.Controllers
 
             await _customerService.DepositAsync(request, customerId);
 
-            return Ok();
+            return Ok("Gracefully deposits amount to customer");
         }
     }
 }
