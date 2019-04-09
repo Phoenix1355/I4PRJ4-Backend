e9a84dec8d5580678a8d15121317212bb9e0d65a
Michael <michael.moeller84@gmail.com>
2019-04-09 11:49:36 +0200

    OpenOrderService now 100% tested

---
+++ SmartCab.Backend/Api.BusinessLogicLayer.UnitTests/Services/OrderServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.BusinessLogicLayer.DataTransferObjects;
+using Api.BusinessLogicLayer.Responses;
+using Api.BusinessLogicLayer.Services;
+using Api.DataAccessLayer.Interfaces;
+using AutoMapper;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Api.BusinessLogicLayer.UnitTests.Services
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        private IMapper _mapper;
+        private IOrderRepository _orderRepository;
+        private OrderService _orderService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = Substitute.For<IMapper>();
+            _orderRepository = Substitute.For<IOrderRepository>();
+            _orderService = new OrderService(_orderRepository, _mapper);
+        }
+
+        [Test]
+        public async Task GetOpenOrdersAsync_WhenCalled_ReturnsExpectedOpenOrdersResponse()
+        {
+            var orderDtos = new List<OrderDto>
+            {
+                new OrderDto {Id = 1, Price = 200, Rides = null, Status = 0},
+                new OrderDto {Id = 2, Price = 400, Rides = null, Status = 0}
+            };
+            _mapper.Map<List<OrderDto>>(null).ReturnsForAnyArgs(orderDtos);
+            var expectedResponse = new OpenOrdersResponse{Orders = orderDtos};
+
+            var response = await _orderService.GetOpenOrdersAsync();
+
+            Assert.That(response.Orders.Count, Is.EqualTo(expectedResponse.Orders.Count));
+        }
+    }
+}
