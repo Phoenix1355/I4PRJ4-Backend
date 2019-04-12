690a33f3bd47b77019f50d779052a8a5c674cc47
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-04-12 15:27:59 +0200

    Added the CreateTaxiCompanyAccount function to allow for integration tests. They all pass.

--- SmartCab.Backend/Api.IntegrationTests/TaxiCompany/LoginTests.cs
+++ SmartCab.Backend/Api.IntegrationTests/TaxiCompany/LoginTests.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Api.BusinessLogicLayer.Responses;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NSubstitute.Extensions;
@@ -12,30 +13,44 @@ namespace Api.IntegrationTests.TaxiCompany
     [TestFixture]
     public class LoginTests : IntegrationSetup
     {
+        private async Task<DataAccessLayer.Models.TaxiCompany> CreateTaxiCompanyAccount()
+        {
+            //Setup database
+            var request = getRegisterRequest();
+            var taxi = new DataAccessLayer.Models.TaxiCompany();
+            await PostAsync("/api/customer/register", request);
+            using (var context = _factory.CreateContext())
+            {
+                //Get data
+                var customer = context.Customers.First();
+                var roleId = context.UserRoles.Where(x => x.UserId == customer.Id).First().RoleId;
+
+                //Set data
+                taxi.PasswordHash = customer.PasswordHash;
+                taxi.Email = customer.Email;
+
+                //Add taxi
+                context.TaxiCompanies.Add(taxi);
+                context.SaveChanges();
+
+                //Add role
+                var userRole = new IdentityUserRole<string>()
+                {
+                    RoleId = roleId,
+                    UserId = taxi.Id
+                };
+                //Save role
+                context.UserRoles.Add(userRole);
+                context.SaveChanges();
+            }
+
+            return taxi;
+        }
+
         [Test]
         public async Task Login_TaxiCompanyExists_LogsInAndReturnsTaxiCompany()
         {
-
-            // Using client to create taxi company
-            var request = getRegisterRequest(email: "test4@gmail.com");
-            await PostAsync("/api/customer/register", request);
-
-            using (var context = _factory.CreateContext())
-            {
-                var customer = context.Customers.First();
-                var customerId = customer.Id;
-                
-                var role = context.Roles.Where(x => x.Name == "TaxiCompany").First();
-                var roleId = role.Id;
-                
-                var customerRole = context.UserRoles.Where(x => x.UserId == customerId).First();
-                context.UserRoles.Remove(customerRole);
-                context.SaveChanges();
-
-                customerRole.RoleId = roleId;
-                context.UserRoles.Add(customerRole);
-                context.SaveChanges();
-            }
+            await CreateTaxiCompanyAccount();
 
             var loginRequest = getLoginRequest();
 
@@ -48,7 +63,8 @@ namespace Api.IntegrationTests.TaxiCompany
         {
             // Using client to create taxi company
             var request = getRegisterRequest();
-            await PostAsync("/api/taxicompany/register", request);
+
+            await CreateTaxiCompanyAccount();
 
             var loginRequest = getLoginRequest();
 
@@ -80,7 +96,8 @@ namespace Api.IntegrationTests.TaxiCompany
         {
             // Using client to create taxi company
             var request = getRegisterRequest();
-            await PostAsync("/api/taxicompany/register", request);
+
+            await CreateTaxiCompanyAccount();
 
             var loginRequest = getLoginRequest("test12@gmail.com", password);
 
@@ -94,7 +111,8 @@ namespace Api.IntegrationTests.TaxiCompany
         {
             // Using client to create taxi company
             var request = getRegisterRequest();
-            await PostAsync("/api/taxicompany/register", request);
+
+            await CreateTaxiCompanyAccount();
 
             var loginRequest = getLoginRequest("test12@gmail.com", "Qwer1!");
 
