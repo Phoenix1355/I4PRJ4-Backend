d4a303e3ec9f2c418b7e7cbd1df4fe543debde1e
Nikolaj R. Molzen <nikolaj_richardy@hotmail.com>
2019-04-27 11:48:42 +0200

    Added datanotations to ensure correct formats in supplied EditRequest

--- SmartCab.Backend/Api/Requests/EditCustomerRequest.cs
+++ SmartCab.Backend/Api/Requests/EditCustomerRequest.cs
@@ -1,9 +1,15 @@
-﻿namespace Api.Requests
+﻿using System.ComponentModel.DataAnnotations;
+using Api.BusinessLogicLayer;
+
+namespace Api.Requests
 {
     public class EditCustomerRequest
     {
+        [StringLength(255, MinimumLength = 3,ErrorMessage = "Name must minimum 3 characters and a maximum of 255 characters.")]
         public string Name { get; set; }
+        [RegularExpression(Constants.EmailRegex, ErrorMessage = Constants.EmailRegexErrorMessage)]
         public string Email { get; set; }
+        [RegularExpression(Constants.PhoneNumberRegex, ErrorMessage = Constants.PhoneNumberRegexErrorMessage)] // 8 digits and cannot start with 0
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
     }
