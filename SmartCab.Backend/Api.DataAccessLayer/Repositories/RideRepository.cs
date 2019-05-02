883de14ba9620f425ee23027d1337d64e7089230
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-05-02 16:34:38 +0200

    Added SharedRideConstraints

--- SmartCab.Backend/Api.DataAccessLayer/Repositories/RideRepository.cs
+++ SmartCab.Backend/Api.DataAccessLayer/Repositories/RideRepository.cs
@@ -37,6 +37,17 @@ namespace Api.DataAccessLayer.Repositories
             return await FindAsync(ride => ride.Status == RideStatus.LookingForMatch);
         }
 
+
+        public void AddSharedRide(Ride ride)
+        {
+            if (ride.PassengerCount > 2)
+            {
+                throw new TooManyPassengersException("SharedRide can only have two passengers");
+            }
+
+            Add(ride);
+        }
+
         /// <summary>
         /// Updates the status of all supplied rides to "Accepted".
         /// </summary>
