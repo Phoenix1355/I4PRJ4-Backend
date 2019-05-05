0f9d6e1565fb9ff29bd6a7a112cbbdd22977ff12
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-05-05 21:11:23 +0200

    First integrations test passed

--- SmartCab.Backend/Api.IntegrationTests/Ride/PriceTests.cs
+++ SmartCab.Backend/Api.IntegrationTests/Ride/PriceTests.cs
@@ -15,5 +15,22 @@ namespace Api.IntegrationTests.Ride
     [TestFixture]
     class PriceTests : IntegrationSetup
     {
+        [Test]
+        public async Task Price_ValidRequest_ReturnsStatusCodeOK()
+        {
+
+            await LoginOnCustomerAccount();
+
+            await DepositToCustomer(1000);
+
+            //Create Ride Request
+            var request = getPriceRequest();
+            
+
+            //Make request
+            var response = await PostAsync("api/rides/price", request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
     }
 }
