3cae8edcf451e0299dc5c3b336f620e5634ab7e7
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-05-05 20:38:13 +0200

    Startet PriceTests.cs

---
+++ SmartCab.Backend/Api.IntegrationTests/Ride/PriceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Api.BusinessLogicLayer.Enums;
+using Api.BusinessLogicLayer.Requests;
+using Api.BusinessLogicLayer.Responses;
+using Api.DataAccessLayer.Models;
+using Api.DataAccessLayer.Statuses;
+using NUnit.Framework;
+
+namespace Api.IntegrationTests.Ride
+{
+
+    [TestFixture]
+    class PriceTests : IntegrationSetup
+    {
+    }
+}
