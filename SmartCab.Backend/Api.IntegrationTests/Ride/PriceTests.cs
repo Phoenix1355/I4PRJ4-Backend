8c7b3e78637bb2b08e5c7e86b82edc4768f193be
Axel Aaby Ulriksen <axelpanorama@hotmail.com>
2019-05-05 21:28:39 +0200

    cleaned up white space

--- SmartCab.Backend/Api.IntegrationTests/Ride/PriceTests.cs
+++ SmartCab.Backend/Api.IntegrationTests/Ride/PriceTests.cs
@@ -18,19 +18,16 @@ namespace Api.IntegrationTests.Ride
         [Test]
         public async Task Price_ValidRequest_ReturnsStatusCodeOK()
         {
-
             await LoginOnCustomerAccount();
 
             await DepositToCustomer(1000);
 
-            //Create Ride Request
             var request = getPriceRequest();
-            
 
-            //Make request
             var response = await PostAsync("api/rides/price", request);
 
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
+
     }
 }
